6477daca412f1cfb68290e5bba208206b0d34bca
Fragsworth <fragsworth@gmail.com>
2013-12-08 00:36:08 -0500

    kind of working

--- numtowif.py
+++ numtowif.py
@@ -1,29 +1,111 @@
 #!/usr/bin/python
 
 import hashlib, binascii
+import ctypes
+import ctypes.util
 
-t='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+ssl = ctypes.cdll.LoadLibrary (ctypes.util.find_library ('ssl') or 'libeay32')
 
-def numtowif(numpriv):
- step1 = '80'+hex(numpriv)[2:].strip('L').zfill(64)
- step2 = hashlib.sha256(binascii.unhexlify(step1)).hexdigest()
- step3 = hashlib.sha256(binascii.unhexlify(step2)).hexdigest()
- step4 = int(step1 + step3[:8] , 16)
- return ''.join([t[step4/(58**l)%58] for l in range(100)])[::-1].lstrip('1')
+def check_result (val, func, args):
+    if val == 0:
+        raise ValueError
+    else:
+        return ctypes.c_void_p (val)
 
-def wiftonum(wifpriv):
- return sum([t.index(wifpriv[::-1][l])*(58**l) for l in range(len(wifpriv))])/(2**32)%(2**256)
+ssl.EC_KEY_new_by_curve_name.restype = ctypes.c_void_p
+ssl.EC_KEY_new_by_curve_name.errcheck = check_result
 
-def validwif(wifpriv):
- return numtowif(wiftonum(wifpriv))==wifpriv
+base58_chars='123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
-keyPhrase = "test 1 2 3"
-nonce = "asdf"
-numAddresses = 10
+def wif_to_address(pkey):
+    # 714 is the identifier OpenSSL uses for the curve "NID_secp256k1".
+    # Verify in the OpenSSL source file crypto/objects/obj_mac.h
+    NID_secp256k1 = 714
+    POINT_CONVERSION_UNCOMPRESSED = 4
+    curve = ssl.EC_KEY_new_by_curve_name(NID_secp256k1)
 
-for i in range(1,numAddresses):
-    privKey = numtowif( int( "0x"+hashlib.sha256(keyPhrase).hexdigest(), 0 ) )
-    print privKey
-    print validwif(privKey)
-    keyPhrase += nonce
+    secret = wif_to_bin(pkey)
+
+    priv_key_bignumber = ssl.BN_bin2bn(secret, 32, ssl.BN_new())
+    group = ssl.EC_KEY_get0_group(curve)
+    pub_key = ssl.EC_POINT_new(group)
+    ctx = ssl.BN_CTX_new()
+    ssl.EC_POINT_mul(group, pub_key, priv_key_bignumber, None, None, ctx)
+    ssl.EC_KEY_set_private_key(curve, priv_key_bignumber)
+    ssl.EC_KEY_set_public_key(curve, pub_key)
+    ssl.EC_POINT_free(pub_key)
+    ssl.BN_CTX_free(ctx)
+    ssl.EC_KEY_set_conv_form(curve, POINT_CONVERSION_UNCOMPRESSED)
+    size = ssl.i2o_ECPublicKey(curve, 0)
+    mb = ctypes.create_string_buffer(size)
+    ssl.i2o_ECPublicKey(curve, ctypes.byref(ctypes.pointer(mb)))
+    pubkey = mb.raw
+
+    ssl.EC_KEY_free(curve)
+
+    # Step 2 - Perform SHA-256 hashing on the public key
+    step2 = hashlib.sha256(pubkey).digest()
+
+    # Step 3 - Perform RIPEMD-160 hashing on the result of SHA-256
+    ripemd160_hasher = hashlib.new('ripemd160')
+    ripemd160_hasher.update(step2)
+    ripemd160 = ripemd160_hasher.digest()
+
+    # Step 4 - Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
+    base58_unencoded_address = add_version_and_checksum(ripemd160, chr(0))
+    base58_encoded_address = bin_to_base58(base58_unencoded_address, 1)
+
+    return base58_encoded_address, ripemd160
+
+def add_version_and_checksum( payload, version ):
+    hashed_twice = hashlib.sha256(hashlib.sha256(version + payload).digest()).digest()
+    return version + payload + hashed_twice[:4]
+
+def bin_to_base58( payload, pad_to_length ):
+    payload_int = int('0x' + payload.encode('hex'), 16)
+    base58_output = ""
+
+    while payload_int > 0:
+        payload_int, r = divmod(payload_int, 58)
+        base58_output = base58_chars[r] + base58_output
+
+    while len(base58_output) < pad_to_length:
+        base58_output = base58_chars[0] + base58_output
+
+    return base58_output
+
+def int_to_wif(private_int):
+    step1 = '80'+hex(private_int)[2:].strip('L').zfill(64)
+    step2 = hashlib.sha256(binascii.unhexlify(step1)).hexdigest()
+    step3 = hashlib.sha256(binascii.unhexlify(step2)).hexdigest()
+    step4 = int(step1 + step3[:8] , 16)
+    return bin_to_base58( int_to_bin(step4), 51 )
+
+def wif_to_int(private_wif):
+    step1 = sum([base58_chars.index(private_wif[::-1][l])*(58**l) for l in range(len(private_wif))])
+    step2 = step1 / (2**32)%(2**256)
+    return step2
+
+def int_to_bin(value):
+    step1 = hex(value)[2:].strip('L')
+    if len(step1) % 2:
+        step1 = '0'+step1
+    return binascii.unhexlify(step1)
+
+def wif_to_bin(private_wif):
+    wif_as_int = wif_to_int(private_wif)
+    wif_as_hex = hex(wif_as_int)[2:].strip('L').zfill(64)
+    return binascii.unhexlify(wif_as_hex)
+
+def is_wif_valid(private_wif):
+    return int_to_wif(wif_to_int(private_wif))==private_wif
+
+
+
+for i in range(1, 10000000):
+    wif = int_to_wif(i)
+    (addr, hex160) = wif_to_address(wif)
+
+    if ( hex160.encode('hex')[0:5] == "00000" ):
+        print i, wif, addr, hex160.encode('hex')
 
--- util.py
+++ util.py
@@ -64,46 +64,12 @@ class KEY:
         else:
             form = self.POINT_CONVERSION_UNCOMPRESSED
         ssl.EC_KEY_set_conv_form(self.k, form)
- 
-def dhash(s):
-    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
- 
-def rhash(s):
-    h1 = hashlib.new('ripemd160')
-    h1.update(hashlib.sha256(s).digest())
-    return h1.digest()
- 
-b58_digits = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
- 
-def base58_encode(n):
-    l = []
-    while n > 0:
-        n, r = divmod(n, 58)
-        l.insert(0,(b58_digits[r]))
-    return ''.join(l)
- 
-def base58_decode(s):
-    n = 0
-    for ch in s:
-        n *= 58
-        digit = b58_digits.index(ch)
-        n += digit
-    return n
- 
-def base58_encode_padded(s):
-    res = base58_encode(int('0x' + s.encode('hex'), 16))
+
+def wif_to_bin(s, version=0):
+    ''' Returns binary representation of the private key '''
     pad = 0
     for c in s:
-        if c == chr(0):
-            pad += 1
-        else:
-            break
-    return b58_digits[0] * pad + res
- 
-def base58_decode_padded(s):
-    pad = 0
-    for c in s:
-        if c == b58_digits[0]:
+        if c == base58_chars[0]:
             pad += 1
         else:
             break
@@ -111,50 +77,48 @@ def base58_decode_padded(s):
     if len(h) % 2:
         h = '0' + h
     res = h.decode('hex')
-    return chr(0) * pad + res
- 
-def base58_check_encode(s, version=0):
-    vs = chr(version) + s
-    check = dhash(vs)[:4]
-    return base58_encode_padded(vs + check)
- 
-def base58_check_decode(s, version=0):
-    k = base58_decode_padded(s)
+    k = chr(0) * pad + res
+
     v0, data, check0 = k[0], k[1:-4], k[-4:]
-    check1 = dhash(v0 + data)[:4]
+    check1 = sha256_twice(v0 + data)[:4]
     if check0 != check1:
         raise BaseException('checksum error')
     if version != ord(v0):
         raise BaseException('version mismatch')
     return data
  
-def gen_eckey(passphrase=None, secret=None, pkey=None, compressed=False, rounds=1, version=0):
-    k = KEY()
-    if passphrase:
-        secret = passphrase.encode('utf8')
-        for i in xrange(rounds):
-            secret = hashlib.sha256(secret).digest()
-    if pkey:
-        secret = base58_check_decode(pkey, 128+version)
-        compressed = len(secret) == 33
-        secret = secret[0:32]
-    k.generate(secret)
-    k.set_compressed(compressed)
-    return k
+def sha256_twice(s):
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
  
-def get_addr(k,version=0):
-    pubkey = k.get_pubkey()
-    secret = k.get_secret()
-    hash160 = rhash(pubkey)
-    addr = base58_check_encode(hash160,version)
-    payload = secret
-    if k.compressed:
-        payload = secret + chr(1)
-    pkey = base58_check_encode(payload, 128+version)
-    return addr, pkey
+def rhash(s):
+    h1 = hashlib.new('ripemd160')
+    h1.update(hashlib.sha256(s).digest())
+    return h1.digest()
+ 
+base58_chars = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+ 
+def base58_encode(n):
+    l = []
+    while n > 0:
+        n, r = divmod(n, 58)
+        l.insert(0,(base58_chars[r]))
+    return ''.join(l)
+ 
+def base58_decode(s):
+    n = 0
+    for ch in s:
+        n *= 58
+        digit = base58_chars.index(ch)
+        n += digit
+    return n
+ 
+def base58_check_encode(s, version=0):
+    vs = chr(version) + s
+    check = sha256_twice(vs)[:4]
+    return base58_encode_padded(vs + check)
  
 def reencode(pkey,version=0):
-    payload = base58_check_decode(pkey,128+version)
+    payload = wif_to_bin(pkey,128+version)
     secret = payload[:-1]
     payload = secret + chr(1)
     pkey = base58_check_encode(payload, 128+version)
@@ -169,12 +133,107 @@ def test(otherversion):
             public = row[1].strip('"')
             private = row[2].strip().strip('"')
  
-            calcpub, calcpriv = get_addr(gen_eckey(pkey=private))
+            calcpub, calcpriv = get_addr(gen_eckey(private))
  
             print index, calcpub == public, calcpriv == private
+ 
+def get_addr(k,version=0):
+    pubkey = k.get_pubkey()
+    secret = k.get_secret()
+    hash160 = rhash(pubkey)
+    addr = base58_check_encode(hash160,version)
+    payload = secret
+    if k.compressed:
+        payload = secret + chr(1)
+    pkey = base58_check_encode(payload, 128+version)
+    return addr, pkey
+ 
+def gen_eckey(pkey):
+    k = KEY()
+    secret = wif_to_bin(pkey, 128)
+    secret = secret[0:32]
+    k.generate(secret)
+    k.set_compressed(False)
+    return k
+
+def base58_encode_padded(s):
+    res = base58_encode(int('0x' + s.encode('hex'), 16))
+    pad = 0
+    for c in s:
+        if c == chr(0):
+            pad += 1
+        else:
+            break
+    return base58_chars[0] * pad + res
+
+def int_to_wif(private_int):
+    step1 = '80'+hex(private_int)[2:].strip('L').zfill(64)
+    step2 = hashlib.sha256(binascii.unhexlify(step1)).hexdigest()
+    step3 = hashlib.sha256(binascii.unhexlify(step2)).hexdigest()
+    step4 = int(step1 + step3[:8] , 16)
+    return ''.join([base58_chars[step4/(58**l)%58] for l in range(100)])[::-1].lstrip('1')
+
+def wif_to_address(pkey):
+    ''' '''
+
+    # 714 is the identifier OpenSSL uses for the curve "NID_secp256k1".
+    # Verify in the OpenSSL source file crypto/objects/obj_mac.h
+    NID_secp256k1 = 714
+    POINT_CONVERSION_UNCOMPRESSED = 4
+    curve = ssl.EC_KEY_new_by_curve_name(NID_secp256k1)
+
+    secret = wif_to_bin(pkey, 128)
+
+    priv_key_bignumber = ssl.BN_bin2bn(secret, 32, ssl.BN_new())
+    group = ssl.EC_KEY_get0_group(curve)
+    pub_key = ssl.EC_POINT_new(group)
+    ctx = ssl.BN_CTX_new()
+    ssl.EC_POINT_mul(group, pub_key, priv_key_bignumber, None, None, ctx)
+    ssl.EC_KEY_set_private_key(curve, priv_key_bignumber)
+    ssl.EC_KEY_set_public_key(curve, pub_key)
+    ssl.EC_POINT_free(pub_key)
+    ssl.BN_CTX_free(ctx)
+    ssl.EC_KEY_set_conv_form(curve, POINT_CONVERSION_UNCOMPRESSED)
+    size = ssl.i2o_ECPublicKey(curve, 0)
+    mb = ctypes.create_string_buffer(size)
+    ssl.i2o_ECPublicKey(curve, ctypes.byref(ctypes.pointer(mb)))
+    pubkey = mb.raw
+
+    # Step 2 - Perform SHA-256 hashing on the public key
+    step2 = hashlib.sha256(pubkey).digest()
+
+    # Step 3 - Perform RIPEMD-160 hashing on the result of SHA-256
+    ripemd160_hasher = hashlib.new('ripemd160')
+    ripemd160_hasher.update(step2)
+    ripemd160 = ripemd160_hasher.digest()
+
+    # Step 4 - Add version byte in front of RIPEMD-160 hash (0x00 for Main Network)
+    base58_unencoded_address = add_version_and_checksum(ripemd160, chr(0))
+    base58_encoded_address = bin_to_base58(base58_unencoded_address)
+
+    return base58_encoded_address
+
+def add_version_and_checksum( payload, version ):
+    return version + payload + sha256_twice(version + payload)[:4]
+
+def bin_to_base58( payload ):
+    n = (int('0x' + payload.encode('hex'), 16))
+    l = []
+    while n > 0:
+        n, r = divmod(n, 58)
+        l.insert(0,(base58_chars[r]))
+    res = ''.join(l)
+    pad = 0
+    for c in payload:
+        if c == chr(0):
+            pad += 1
+        else:
+            break
+    return base58_chars[0] * pad + res
    
 def generate_from_privkey(key):
-    return get_addr(gen_eckey(pkey=key))
+    #return get_addr(gen_eckey(pkey=key))
+    return wif_to_address(key)
  
    
 if False and __name__ == '__main__':
@@ -186,13 +245,15 @@ if False and __name__ == '__main__':
  
     test(int(options.otherversion))
 
-print generate_from_privkey("5KgvTTyepNPd8z8TsMQ1c4vi2jXqH1kb3nokAFEUXTV3ezxFR4U")
-#print get_addr(eckey);
-print "Should be 13RBwAY42qJLwRJYUAqee7AnGtRzk9kAyM"
-
-
-
-
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
+print wif_to_address("5HpHagT65TZzG1PH3CSu63k8DbpvD8s5ip4nEB3kEsreAnchuDf")
 
 
 
